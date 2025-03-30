8fe15b449e27eb4b5041932cd384dd899b1c9245
mee7ya <bas9339@gmail.com>
2025-03-30 20:56:21 +0300

    Migrate to svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import '../app.css';
+	import Particles, { particlesInit } from '@tsparticles/svelte';
+	import { loadSlim } from '@tsparticles/slim';
+
+	let particlesConfig = {
+		background: {
+			color: {
+				value: '#141414'
+			}
+		},
+		particles: {
+			color: {
+				value: '#ffffff'
+			},
+			links: {
+				color: '#ffffff',
+				distance: 300,
+				enable: true,
+				opacity: 0.3,
+				width: 1
+			},
+			move: {
+				direction: 'none' as const,
+				enable: true,
+				outModes: 'bounce' as const,
+				random: false,
+				speed: 3,
+				straight: false
+			},
+			number: {
+				density: {
+					enable: true
+				},
+				value: 40
+			},
+			opacity: {
+				value: 0.5
+			},
+			shape: {
+				type: 'circle'
+			},
+			size: {
+				value: { min: 2, max: 5 }
+			}
+		}
+	};
+
+	void particlesInit(async (engine) => {
+		await loadSlim(engine);
+	});
+
+	let { children } = $props();
+</script>
+
+<Particles id="tsparticles" class="relative top-0 left-0 -z-10" options={particlesConfig} />
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import MTButton from '../components/mt_button.svelte';
+	import Telegram from '../assets/telegram.svelte';
+	import GitHub from '../assets/github.svelte';
+	import LinkedIn from '../assets/linkedin.svelte';
+	import HabrCareer from '../assets/habr_career.svelte';
+	import Email from '../assets/email.svelte';
+
+	import {
+		PUBLIC_TELEGRAM_LINK,
+		PUBLIC_GITHUB_LINK,
+		PUBLIC_LINKEDIN_LINK,
+		PUBLIC_HABR_CAREER_LINK,
+		PUBLIC_EMAIL_LINK
+	} from '$env/static/public';
+</script>
+
+{#snippet telegram()}
+	<Telegram></Telegram>
+{/snippet}
+
+{#snippet github()}
+	<GitHub></GitHub>
+{/snippet}
+
+{#snippet linkedin()}
+	<LinkedIn></LinkedIn>
+{/snippet}
+
+{#snippet habrCareer()}
+	<HabrCareer></HabrCareer>
+{/snippet}
+
+{#snippet email()}
+	<Email></Email>
+{/snippet}
+
+<div class="flex min-h-screen items-center justify-center">
+	<div class="flex flex-col gap-y-2.5">
+		<MTButton icon={telegram} link={PUBLIC_TELEGRAM_LINK} text="Telegram"></MTButton>
+		<MTButton icon={github} link={PUBLIC_GITHUB_LINK} text="GitHub"></MTButton>
+		<MTButton icon={linkedin} link={PUBLIC_LINKEDIN_LINK} text="LinkedIn"></MTButton>
+		<MTButton icon={habrCareer} link={PUBLIC_HABR_CAREER_LINK} text="HabrCareer"></MTButton>
+		<MTButton icon={email} link={PUBLIC_EMAIL_LINK} text="E-mail"></MTButton>
+	</div>
+</div>
